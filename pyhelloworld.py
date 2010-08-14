dc6de1fd627e0c6d8f4bb8ac2ab82193f6a8f576
Jrabbit <jackjrabbit+github@gmail.com>
2010-08-13 20:40:15 -0500

    Hide after push

--- pyhelloworld.py
+++ pyhelloworld.py
@@ -1,14 +1,32 @@
 from pyjamas.ui.Button import Button
 from pyjamas.ui.RootPanel import RootPanel
 from pyjamas.ui.HTML import HTML
+from pyjamas.ui.Image import Image
+# from pyjamas.ui.SimplePanel import SimplePanel
 from pyjamas import Window
+import pyjslib
+from __pyjamas__ import JS
 
 def start(sender):
     Window.alert("Hello, GAMERS!")
-    hw = HTML("<img src='http://github.com/jrabbit/Defenditt/raw/master/media/instructions%20screen.png' alt='instructions'/>")
+    hw = HTML("<img src='http://github.com/jrabbit/Defenditt/raw/master/media/instructions%20screen.png' alt='instructions'/>", StyleName="instructions")
     RootPanel().add(hw)
+    JS(""" 
+    parent.top.document.getElementById("splash").style.display = "none"; 
+    parent.top.document.getElementById("startbutton").style.display = "none";
+    parent.top.document.getElementById("startbutton").style.height = 0;
+    """)
+
+def defenditt():
+    #Characters
+    snoo = Image("./public/media/alien.png")
+    suit = Image("./public/media/suit.png")
+    pass
+
 if __name__ == '__main__':
     b = Button("Start Game", start)
+    b.setID('startbutton')
     RootPanel().add(b)
     hw = HTML("<img src='http://github.com/jrabbit/Defenditt/raw/master/media/splash%20screen.png' alt='start screen'/>")
+    hw.setID("splash")
     RootPanel().add(hw)
