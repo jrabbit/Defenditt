88ca4b548502c395e2f94ac066bedcbb4dc8ff91
Jrabbit <jackjrabbit+github@gmail.com>
2010-08-14 00:31:00 -0500

    able to advance to game bkg

--- canvas.py
+++ canvas.py
@@ -6,36 +6,34 @@ from pyjamas.ui.Image import Image
 Alien_ypos = 125
 difficulty = 1
 
-class defenddit:
-    def __init__(self):
-        pass
-
-def move_alien(y):
-    """move the alien to the y coord: following the mouse"""
-    pass
-
-def bang():
-    """fire a bullet at the alien's current y position"""
-    pass
+# class defenddit:
+#     def __init__(self):
+#         pass
+# 
+# def move_alien(y):
+#     """move the alien to the y coord: following the mouse"""
+#     pass
+# 
+# def bang():
+#     """fire a bullet at the alien's current y position"""
+#     pass
 
 class OurCanvas(Canvas):
     def __init__(self):
         Canvas.__init__(self, 300, 250)
         self.img = CanvasImage('media/splash%20screen.png', self)
+        self.screen = 1
         self.addMouseListener(self)
     
     def onMouseDown(self, sender, x, y):
-        if instuctions is not 1:
-             self.img = CanvasImage('media/instructions%20screen.png', self)
-             instructions = 1
-        elif game is not 1:
-            self.img = CanvasImage('media/background.png', self)
-            game = 1
-            defenddit()
-        else:
-            #BANG!
-            bang()
-            pass
+        self.img = CanvasImage('media/instructions%20screen.png', self)
+        RootPanel().add(HTML("mouse down instuctions"))
+        RootPanel().add(HTML(self.screen))
+        if self.screen >= 2:
+            self.img = CanvasImage('media/bkg.png', self)
+            #RootPanel().add(HTML("mouse down bkg"))
+        self.screen = self.screen + 1
+    
 
     def onMouseEnter(self, sender):
         #RootPanel().add(HTML("mouseenter: setting focus (keyboard input accepted)"))      
@@ -47,7 +45,7 @@ class OurCanvas(Canvas):
 
     def onMouseMove(self, sender, x, y):
         #RootPanel().add(HTML("move: x %d " % x + "y %d" % y))
-        move_alien(y)
+        #move_alien(y)
         pass
     
     def onMouseUp(self, sender, x, y):
