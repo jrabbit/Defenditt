cb9b088a7ed4d82549e0745965019abb3fd30102
Jrabbit <jackjrabbit+github@gmail.com>
2010-08-15 17:03:26 -0500

    end of game

--- canvas.py
+++ canvas.py
@@ -6,17 +6,17 @@ from pyjamas.ui.Image import Image
 Alien_ypos = 125
 difficulty = 1
 
-class defenddit(Canvas):
-    def __init__(self):
-        pass
-
-def move_alien(y):
-    """move the alien to the y coord: following the mouse"""
-    pass
-
-def bang():
-    """fire a bullet at the alien's current y position"""
-    pass
+# class defenddit(Canvas):
+#     def __init__(self):
+#         pass
+# 
+# def move_alien(y):
+#     """move the alien to the y coord: following the mouse"""
+#     pass
+# 
+# def bang():
+#     """fire a bullet at the alien's current y position"""
+#     pass
 
 class OurCanvas(Canvas):
     def __init__(self):
@@ -26,7 +26,9 @@ class OurCanvas(Canvas):
         self.addMouseListener(self)
     
     def onMouseDown(self, sender, x, y):
-        images = ['instructions%20screen.png', 'titlecard.png' ]
+        images = ['instructions%20screen.png', 'titlecard.png', 'scene 1.png', 'scene 1 click.png',
+        'scene 2 .png', 'scene 2 cl1.png', 'scene 2 cl2.png', 'scene 3.png',
+        'scene 3.png', 'scene 3 cl.png', 'end.png' ]
         self.img = CanvasImage("media/" + images[self.screen], self)
         
         # if self.screen == 1:
@@ -57,7 +59,7 @@ class OurCanvas(Canvas):
     
     def onMouseMove(self, sender, x, y):
         #RootPanel().add(HTML("move: x %d " % x + "y %d" % y))
-        move_alien(y)
+        # move_alien(y)
         pass
     
     def onMouseUp(self, sender, x, y):
