f4eac3756944f9bf6e9d2a2d9134199fcf9a0253
Jrabbit <jackjrabbit+github@gmail.com>
2010-08-14 21:33:44 -0500

    BLINKING ALIEN FOR GREAT JUSTICE.

--- canvas.py
+++ canvas.py
@@ -6,17 +6,17 @@ from pyjamas.ui.Image import Image
 Alien_ypos = 125
 difficulty = 1
 
-# class defenddit:
-#     def __init__(self):
-#         pass
-# 
-# def move_alien(y):
-#     """move the alien to the y coord: following the mouse"""
-#     pass
-# 
-# def bang():
-#     """fire a bullet at the alien's current y position"""
-#     pass
+class defenddit(Canvas):
+    def __init__(self):
+        pass
+
+def move_alien(y):
+    """move the alien to the y coord: following the mouse"""
+    pass
+
+def bang():
+    """fire a bullet at the alien's current y position"""
+    pass
 
 class OurCanvas(Canvas):
     def __init__(self):
@@ -33,6 +33,11 @@ class OurCanvas(Canvas):
             
         if self.screen == 2:
             self.img = CanvasImage('media/bkg.png', self)
+            self.alien = CanvasImage('media/reddit alien.png')
+            self.loader = ImageLoadListener()
+            self.loader.add(self.alien)
+            self.context.drawImage(self.alien.getElement(), 0, Alien_ypos)
+            self.context.globalCompositeOperation('source-atop')
             #self.screen = self.screen + 1
             #RootPanel().add(HTML("mouse down bkg"))
         self.screen = self.screen + 1
@@ -42,14 +47,14 @@ class OurCanvas(Canvas):
     def onMouseEnter(self, sender):
         #RootPanel().add(HTML("mouseenter: setting focus (keyboard input accepted)"))      
         self.setFocus(True)
-
+    
     def onMouseLeave(self, sender):
         #RootPanel().add(HTML("mouseleave: clearing focus (keyboard input not accepted)"))      
         self.setFocus(False)
-
+    
     def onMouseMove(self, sender, x, y):
         #RootPanel().add(HTML("move: x %d " % x + "y %d" % y))
-        #move_alien(y)
+        move_alien(y)
         pass
     
     def onMouseUp(self, sender, x, y):
